79d8fe97c1b9363741c2b2207d4c1f91d77b113e
imnazmul <nazmulislam2322@gmail.com>
2024-11-26 01:02:00 +0600

    settings page completed

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,7 +1,29 @@
-import { View } from "react-native";
+import { View, Text, Pressable } from "react-native";
+import {MaterialIcons} from "@expo/vector-icons"
+import Colors from "../configs/Colors";
 
 const Settings = () => {
-    return (<View>
+    return (<View className="my-6 flex-1">
+
+        <View className="mx-2 p-4 shadow rounded bg-white">
+
+        <Pressable android_ripple={{color: Colors.gray50}} className="flex-row items-center gap-4 p-4 mb-2">
+            <View className="w-6"><MaterialIcons size={24} name="star-rate" /></View>
+            <Text className="text-lg">Rate Us</Text>
+        </Pressable>
+        <Pressable android_ripple={{color: Colors.gray50}} className="flex-row items-center gap-4 p-4 mb-2">
+            <View className="w-6"><MaterialIcons size={24} name="privacy-tip" /></View>
+            <Text className="text-lg">Privacy Policy</Text>
+        </Pressable>
+        <Pressable android_ripple={{color: Colors.gray50}} className="flex-row items-center gap-4 p-4 mb-2">
+            <View className="w-6"><MaterialIcons size={24} name="policy" /></View>
+            <Text className="text-lg">Terms & Conditions</Text>
+        </Pressable>
+
+        
+
+        </View>
+
 
     </View>)
 }
